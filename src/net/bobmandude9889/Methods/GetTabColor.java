783ae995bd7dcf7f855cb9fbd5d5b3c6d9a16093
unknown <willgarrett610@gmail.com>
2014-06-12 16:24:14 -0600

    Fixed tab colors

--- src/net/bobmandude9889/Methods/GetTabColor.java
+++ src/net/bobmandude9889/Methods/GetTabColor.java
@@ -7,35 +7,35 @@ public class GetTabColor {
 	public static ChatColor getTabColor(Player player) {
 		if (player.hasPermission("tabcol.0"))
 			return ChatColor.BLACK;
-		else if (player.hasPermission("tabcol.1"))
+		if (player.hasPermission("tabcol.1"))
 			return ChatColor.DARK_BLUE;
-		else if (player.hasPermission("tabcol.2"))
+		if (player.hasPermission("tabcol.2"))
 			return ChatColor.DARK_GREEN;
-		else if (player.hasPermission("tabcol.3"))
+		if (player.hasPermission("tabcol.3"))
 			return ChatColor.DARK_AQUA;
-		else if (player.hasPermission("tabcol.4"))
+		if (player.hasPermission("tabcol.4"))
 			return ChatColor.DARK_RED;
-		else if (player.hasPermission("tabcol.5"))
+		if (player.hasPermission("tabcol.5"))
 			return ChatColor.DARK_PURPLE;
-		else if (player.hasPermission("tabcol.6"))
+		if (player.hasPermission("tabcol.6"))
 			return ChatColor.GOLD;
-		else if (player.hasPermission("tabcol.7"))
+		if (player.hasPermission("tabcol.7"))
 			return ChatColor.GRAY;
-		else if (player.hasPermission("tabcol.8"))
+		if (player.hasPermission("tabcol.8"))
 			return ChatColor.DARK_GRAY;
-		else if (player.hasPermission("tabcol.9"))
+		if (player.hasPermission("tabcol.9"))
 			return ChatColor.BLUE;
-		else if (player.hasPermission("tabcol.a"))
+		if (player.hasPermission("tabcol.a"))
 			return ChatColor.GREEN;
-		else if (player.hasPermission("tabcol.b"))
+		if (player.hasPermission("tabcol.b"))
 			return ChatColor.AQUA;
-		else if (player.hasPermission("tabcol.c"))
+		if (player.hasPermission("tabcol.c"))
 			return ChatColor.RED;
-		else if (player.hasPermission("tabcol.d"))
+		if (player.hasPermission("tabcol.d"))
 			return ChatColor.LIGHT_PURPLE;
-		else if (player.hasPermission("tabcol.e"))
+		if (player.hasPermission("tabcol.e"))
 			return ChatColor.YELLOW;
-		else if (player.hasPermission("tabcol.f"))
+		if (player.hasPermission("tabcol.f"))
 			return ChatColor.WHITE;
 		return ChatColor.RESET;
 	}
